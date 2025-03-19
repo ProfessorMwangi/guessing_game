e50ff9198e584cd2a6c54dc063f05139e286c952
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2025-03-20 02:42:53 +0300

    Adds a guessing game loop
    Improves the number guessing game by adding a loop that repeatedly prompts the user for input until the correct number is guessed.
    Handles invalid input gracefully by continuing the loop.

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,29 @@ use rand::Rng;
 use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess...");
+    loop{
 
     let mut guess = String::new();
+
     let secret_number= rand::rng().random_range(1..=100);
+
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
-    let guess:u32 =guess.trim().parse().expect("Please type a number!");
+
+    let guess:u32 = match guess.trim().parse(){
+        Ok(num)=>num,
+        Err(_)=>continue,
+    };
+    
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!.Secret number is = {}",secret_number),
-        Ordering::Greater => println!("Too big!Secret number is = {}",secret_number),
-        Ordering::Equal => println!("You win!Secret number is = {}",secret_number),
-    }
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => {
+            println!("You win!");
+            break;
+        }
+    }}
 }
 
